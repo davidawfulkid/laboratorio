93e7af083ec33a7a9077c5b27f728ad71f49490d
davideverywhere <00jackhack11@gmail.com>
2025-03-27 18:32:22 -0500

    metodo test1, creacion de proyecto y archivo de diagrama de clases

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
+import com.laboratorio.test1;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
+
+        test1 mensaje = new test1();
+        mensaje.mensajePrueba();
     }
+
+    
 }
