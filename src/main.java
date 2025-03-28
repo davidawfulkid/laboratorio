72213db270d9c1643c11872792456dbc9e71481c
NikollMoralesL <nikoll.moralesl@uqvirtual.edu.co>
2025-03-27 22:37:12 -0500

    creacion de clase test3 con metodo que imprime mi nombre

--- src/main.java
+++ src/main.java
@@ -3,8 +3,6 @@ import com.laboratorio.test1;
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
-        test2 santiago = new test2();
-        santiago.mostrarMensajeSantiago();
     }
 
     
