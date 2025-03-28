19e965443fbf777d02aa5f15ae286eafbbd1c353
jamaicatc <santiagofernan87@gmail.com>
2025-03-28 12:31:25 -0500

    rama de nicolas_Toro, incorrectamente agregada, se revierte para posteriormente volverla a crear
    This reverts commit 5061e819d48e4b7aa7418c6bb9945f62db72c4ee, reversing
    changes made to ee663abbac326c1efec740c3a9a3b455871fe611.

--- src/com/laboratorio/main.java
+++ src/com/laboratorio/main.java
@@ -2,10 +2,9 @@ package com.laboratorio;
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
-
         test1 mensaje = new test1();
         mensaje.mensajePrueba();
-        
+
         test2 santiago = new test2();
         santiago.mostrarMensajeSantiago();
 
