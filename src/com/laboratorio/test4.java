563798de1af0c7de81a76c5a54ea917949d9a653
Nicolas Toro <kodaxzzz@gmail.com>
2025-03-28 14:17:40 -0500

    creacion de metodo test4, y metodo que imprime mi nombre

---
+++ src/com/laboratorio/test4.java
@@ -0,0 +1,9 @@
+package com.laboratorio;
+
+public class test4 {
+    public void mostrarMensaje(){
+        System.out.println("---------------------------------------------------");
+        System.out.println("hola, mi nombre es nicolas toro y este es mi metodo");
+        System.out.println("---------------------------------------------------");
+    }
+}
