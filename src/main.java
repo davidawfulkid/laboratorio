ad2e29e035eb4f484563f2e0ccf875b5a16c63ee
NikollMoralesL <nikoll.moralesl@uqvirtual.edu.co>
2025-03-27 22:37:12 -0500

    creacion de clase test3 con metodo que imprime mi nombre

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
+        test3 nikoll = new test3();
+        nikoll.mensajeNikoll();
     }
 }
---
+++ src/test3.java
@@ -0,0 +1,7 @@
+public class test3 {
+    public void mensajeNikoll(){
+        System.out.println("------------------");
+        System.out.println("este es el mensaje de Nikoll");
+        System.out.println("------------------");
+    }
+}
