8947c8f1aa76c4439ce5094f560b3e7f2330e4f0
jamaicatc <santiagofernan87@gmail.com>
2025-03-27 23:06:43 -0500

    creacion de clase test2, con metodo saludo

--- src/main.java
+++ src/main.java
@@ -3,6 +3,8 @@ import com.laboratorio.test1;
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
+        test2 santiago = new test2();
+        santiago.mostrarMensajeSantiago();
     }
 
     
