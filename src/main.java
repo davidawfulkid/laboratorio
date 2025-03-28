ee663abbac326c1efec740c3a9a3b455871fe611
jamaicatc <santiagofernan87@gmail.com>
2025-03-28 00:31:28 -0500

    creacion de clase y metodo de nicolas toro

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Bienvenidos al laboratorio de Git con Java");
+        test4 toro = new test4();
+        toro.mensajeToro();
     }
 }
